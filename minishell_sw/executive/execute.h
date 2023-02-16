990efd36292dffa5ff51b2feda72be72ce7ed875
도준웅 <jdoh@dojun-ung-ui-MacBookAir.local>
2023-02-16 22:16:04 +0900

    pipe, command 부분수정

--- minishell_sw/executive/execute.h
+++ minishell_sw/executive/execute.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sunwsong <sunwsong@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jdoh <jdoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:59:29 by sunwsong          #+#    #+#             */
-/*   Updated: 2023/02/14 17:14:30 by sunwsong         ###   ########.fr       */
+/*   Updated: 2023/02/16 21:54:35 by jdoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 typedef struct s_pipe_info
 {
 	pid_t	*pid_table;
+	char	**path_split;
 	int		(*fds)[2];
 	int		process_cnt;
 }	t_pipe_info;
@@ -29,8 +30,8 @@ typedef struct s_pipe_info
 // execute_utils
 char		**compound_to_char_twoptr(t_tree *cur);
 char		*expand_char(t_list *compound_list);
-void		find_path(char **cmd);
-void		access_check(char *cmd, char mode);
+void		add_path_and_access_check(char **path_split, char **cmd);
+char		**make_path_split(void);
 
 // execute_pipe
 void		init_pipeinfo(t_pipe_info *info, t_tree *cur);
