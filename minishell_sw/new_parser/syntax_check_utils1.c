a847dcc8a620d37265597b1befa799e7bb229603
dandb3 <98573005+dandb3@users.noreply.github.com>
2023-02-21 10:50:30 +0900

    fixing..

--- minishell_sw/new_parser/syntax_check_utils1.c
+++ minishell_sw/new_parser/syntax_check_utils1.c
@@ -21,54 +21,66 @@ void	syntax_init(t_node **cur_token, t_list **stack, t_list *token_list)
 	push(*stack, make_tree(AST_E0));
 }
 
-static void	sigaction_init(struct sigaction *sa)
+static void	sigaction_heredoc(struct sigaction *sa, struct sigaction *old_sa)
 {
-	ft_memset(sa, 0, sizeof(sa));
+	ft_memset(sa, 0, sizeof(*sa));
 	sa->__sigaction_u.__sa_handler = heredoc_handler;
 	sa->sa_flags = 0;
 	sigaddset(&sa->sa_mask, SIGINT);
+	sigaction(SIGINT, sa, old_sa);
 }
 
-static char	*read_here_doc(char *here_doc_end)
+static char	*read_heredoc_line(char *heredoc_end)
 {
-	char	buf[1025];
-	char	*total_line;
-	ssize_t	read_len;
+	static char	buf[1025];
+	char		*total_line;
+	size_t		total_len;
+	ssize_t		read_len;
 
-	buf[1024] = '\0';
+	total_len = 0;
+	read_len = 1;
 	total_line = ft_strdup("");
 	if (total_line == NULL)
 		exit(MALLOC_FAILURE);
-	read_len = read(STDIN_FILENO, buf, 1024);
-	while (read_len > 0 && ft_strchr(buf, '\n') == NULL \
-		&& ft_strcmp(buf, here_doc_end))
+	while ((total_len | read_len) != 0 && ft_strcmp(total_line, heredoc_end) \
+		   && ft_strchr(total_line, '\n') == NULL)
 	{
-		total_line = ft_strjoin_and_free(total_line, buf);
 		read_len = read(STDIN_FILENO, buf, 1024);
+		if (read_len == FAILURE)
+		{
+			set_heredoc_status(1);
+			return ((void *)free_ret(total_line, NULL, NULL, 0));
+		}
+		total_len += (size_t) read_len;
+		total_line = ft_strjoin_and_free(total_line, buf);
 	}
-	if (read_len == FAILURE)
-		set_heredoc_status(1);
+	if ((total_len | read_len) == 0 || !ft_strcmp(total_line, heredoc_end))
+		return ((void *)free_ret(total_line, NULL, NULL, 0));
 	return (total_line);
 }
 
 void	here_doc_or_pop(t_list *stack, t_node **cur_token)
 {
-	char				*here_doc_end;
 	struct sigaction	sa;
 	struct sigaction	old_sa;
+	char				*heredoc_end;
+	char				*cur_line;
 
 	pop_tree(stack);
 	if ((*cur_token)->lex == LEX_COMPOUND \
 		&& (*cur_token)->prev->lex == LEX_HERE_DOC)
 	{
-		sigaction_init(&sa);
-		sigaction(SIGINT, &sa, &old_sa);
+		sigaction_heredoc(&sa, &old_sa);
 		del_quotes((*cur_token)->val);
-		here_doc_end = extract_pure_word((*cur_token)->val);
+		heredoc_end = ft_strjoin_and_free(extract_pure_word((*cur_token)->val), "\n");
 		free_list((*cur_token)->val, 0, LEX);
-		(*cur_token)->val = read_here_doc(here_doc_end);
 		(*cur_token)->lex = LEX_WORD;
-		free(here_doc_end);
+		(*cur_token)->val = ft_strdup("");
+		if ((*cur_token)->val == NULL)
+			exit(MALLOC_FAILURE);
+		
+		cur_line = read_heredoc_line(heredoc_end);
+		free(heredoc_end);
 		sigaction(SIGINT, &old_sa, NULL);
 	}
 	*cur_token = (*cur_token)->next;
